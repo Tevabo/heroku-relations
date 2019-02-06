73988e4d0a9e252cd1ace747a7868b67516fa03a
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-06 14:22:07 +0100

    JWT now catches errors

--- auth/routes.js
+++ auth/routes.js
@@ -18,4 +18,27 @@ router.post('/logins', (req, res) => {
   }
 })
 
+router.get('/secret-endpoint', (req, res) => {
+  const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
+    try {
+    const data = toData(auth[1])
+    res.send({
+      message: 'Thank you for visiting the secret endpoint.',
+      data
+    })
+  }
+  catch(error) {
+    res.status(400).send({
+      message: `Error ${error.name}: ${error.message}`
+    })
+  }
+}
+  else {
+    res.status(401).send({
+      message: 'Please supply some valid credentials'
+    })
+  }
+})
+
 module.exports = router
