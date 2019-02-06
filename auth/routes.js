0fced604f0a8e9a03d0559d07bb7181c77a617be
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-06 15:05:38 +0100

    Can now create user with encrypted password and login with credentials

--- auth/routes.js
+++ auth/routes.js
@@ -1,23 +1,48 @@
-const { toJWT, toData} = require('./jwt')
+const { toJWT, toData } = require('./jwt')
 const { Router } = require('express')
+const User = require('../users/model')
+const bcrypt = require('bcrypt')
 
 const router = new Router()
 
 
 router.post('/logins', (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password) {
-      return res.status(400).send({
-            message: `Please supply a valid email and password`
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).send({
+      message: `Please supply a valid email and password`
     })
   }
   else {
-    // 1. find user based on email address
-    // 2. use bcrypt.compareSync to check the password against the stored hash
-    // 3. if the password is correct, return a JWT with the userId of the user (user.id)
-    res.send({
-      jwt: toJWT({ userId: 1 })
-    })
+    User
+      .findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      .then(entity => {
+        if (!entity) {
+          res.status(400).send({
+            message: 'User with that email does not exist'
+          })
+        }
+        if (bcrypt.compareSync(req.body.password, entity.password)) {
+          res.send({
+            jwt: toJWT({ userId: entity.id })
+          })
+        }
+        else {
+          res.status(400).send({
+            message: 'Password was incorrect'
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send({
+          message: 'Oops! Something went wrong'
+        })
+      })
   }
 })
 
@@ -25,18 +50,18 @@ router.get('/secret-endpoint', (req, res) => {
   const auth = req.headers.authorization && req.headers.authorization.split(' ')
   if (auth && auth[0] === 'Bearer' && auth[1]) {
     try {
-    const data = toData(auth[1])
-    res.send({
-      message: 'Thank you for visiting the secret endpoint.',
-      data
-    })
+      const data = toData(auth[1])
+      res.send({
+        message: 'Thank you for visiting the secret endpoint.',
+        data
+      })
+    }
+    catch (error) {
+      res.status(400).send({
+        message: `Error ${error.name}: ${error.message}`
+      })
+    }
   }
-  catch(error) {
-    res.status(400).send({
-      message: `Error ${error.name}: ${error.message}`
-    })
-  }
-}
   else {
     res.status(401).send({
       message: 'Please supply some valid credentials'
